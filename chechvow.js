617cbf9ffec938f0c14220f721cfe702b7b68164
Hashemeee92 <hashemeee92@gmail.com>
2025-08-02 22:12:41 +0600

    update string

--- chechvow.js
+++ chechvow.js
@@ -1,18 +1,18 @@
 // JavaScript implementation of checking if a string contains all vowels
 
 // Given string
-let S = "Abulhashemio";
+let Str = "Abulhashemio";
 
 // Convert the string to lowercase to make the comparison case-insensitive
-S = S.toLowerCase();
+Str = Str.toLowerCase();
 
 // Check if S contains all vowels using the includes() method and if statement
 if (
-  S.includes("a") &&
-  S.includes("e") &&
-  S.includes("i") &&
-  S.includes("o") &&
-  S.includes("u")
+  Str.includes("a") &&
+  Str.includes("e") &&
+  Str.includes("i") &&
+  Str.includes("o") &&
+  Str.includes("u")
 ) {
   // All vowels are present in the string
   console.log("All vowels are present in the string");
