ab67755b579bd57a315ce0f7807ac90d47e395ab
Hashemeee92 <hashemeee92@gmail.com>
2025-08-02 22:28:32 +0600

    update code

--- check.vow.js
+++ check.vow.js
@@ -1,20 +1,21 @@
 // Get a string to check vowels present or not
-let str = "Aemisscountry";
-// Conver to lower case
-str=str.toLowerCase();
+let str = "Amisscountry";
+// Convert to lower case
+str = str.toLowerCase();
 //check vowels whether present or not
 if(
-    str.includes("a") &&;
-    str.includes("e") &&;
-    str.includes("i") &&;
-    str.includes("o") &&;
+    str.includes("a") &&
+    str.includes("e") &&
+    str.includes("i") &&
+    str.includes("o") &&
     str.includes("u")
 )
+//if all vowels present the print 
 {
-    //if all vowels present the print 
-    console.log("All vowels are present int string");
+    console.log("All vowels are present int string")
 }
+ // all vowels are not present
 else{
-    // all vowels are not present
+   
     console.log("All vowels are not present in the string")
 }
--- conv.js
+++ conv.js
@@ -1,4 +1,4 @@
-let s = 'a';
+let s = 'b';
 if (s === 'a' || s === 'e' || s === 'i' || s === 'o' || s === 'u' ||
     s === 'A' || s === 'E' || s === 'I' || s === 'O' || s === 'U') {
     console.log(`${s} is a vowel`);
