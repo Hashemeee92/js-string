d72669a70e619992c9cab50da347266d44a246ac
Hashemeee92 <hashemeee92@gmail.com>
2025-08-02 19:38:07 +0600

    Find the number of consonant

--- consonant.js
+++ consonant.js
@@ -2,9 +2,9 @@ let str= "Bangladesh";
 let cnt = 0;
 for(let i=0; i<str.length; i++){
     let char=str.charAt(i).toLowerCase();
-    if(char>='a'  && char<='z' && !'bcdfghjklmnpqrstvwxyz'.includes(char)){
+    if(char>='a'  && char<='z' && !'aeiou'.includes(char)){
         cnt++;
 
     }
 }
-console.log("Number of vowels:",cnt)
+console.log("Number of consonant:",cnt)
