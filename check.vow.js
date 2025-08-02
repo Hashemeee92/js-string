d28f5b93608e17370ad80dfc25191a485fdc1056
Hashemeee92 <hashemeee92@gmail.com>
2025-08-02 22:20:41 +0600

    check vowels

---
+++ check.vow.js
@@ -0,0 +1,20 @@
+// Get a string to check vowels present or not
+let str = "Aemisscountry";
+// Conver to lower case
+str=str.toLowerCase();
+//check vowels whether present or not
+if(
+    str.includes("a") &&;
+    str.includes("e") &&;
+    str.includes("i") &&;
+    str.includes("o") &&;
+    str.includes("u")
+)
+{
+    //if all vowels present the print 
+    console.log("All vowels are present int string");
+}
+else{
+    // all vowels are not present
+    console.log("All vowels are not present in the string")
+}
